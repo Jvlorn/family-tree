ebbafb81209a276918de22dce699cf4c0cd16c5d
Jacob Cacayan <143243791+Jvlorn@users.noreply.github.com>
2026-05-16 03:36:47 +0800

    Update SUPABASE_URL to remove endpoint path

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-const SUPABASE_URL = 'https://uwypbyjckslbtqasvfju.supabase.co/rest/v1/';
+const SUPABASE_URL = 'https://uwypbyjckslbtqasvfju.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV3eXBieWpja3NsYnRxYXN2Zmp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc3ODkyOTMsImV4cCI6MjA5MzM2NTI5M30.lxbSI8L3JI7ZZmPj8-B6oi424gSbuIsye9WJLjJdV0w';
